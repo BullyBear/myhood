58771d7c3ef282dd02e723e7eec37810929af0c0
BullyBear <w.stevens1787@gmail.com>
2023-10-06 12:13:06 -0400

    forgot chatscreen

--- FrontEnd/src/screens/ChatScreen.js
+++ FrontEnd/src/screens/ChatScreen.js
@@ -20,10 +20,12 @@ const ChatScreen = ({ route }) => {
           <Text style={styles.sendButtonText}>Send</Text>
         </TouchableOpacity>
       </View> */}
-      
+
     </View>
   );
 };
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -35,38 +37,50 @@ const styles = StyleSheet.create({
     padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
+    elevation: 5,  // for Android
+    shadowOpacity: 0.3, // for iOS
+    shadowRadius: 2,    // for iOS
+    shadowOffset: { height: 2, width: 0 },  // for iOS
   },
   footer: {
     flexDirection: 'row',
     backgroundColor: '#fff',
     paddingHorizontal: 10,
-    paddingVertical: 5,
+    paddingVertical: 10,
     borderTopColor: '#ccc',
     borderTopWidth: 1,
   },
   text: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 22,  // slightly larger for prominence
     fontWeight: 'bold',
   },
   input: {
     flex: 1,
-    height: 40,
-    borderColor: '#ccc',
+    height: 45,  // slightly taller for better touch targets
+    borderColor: '#007BFF',
     borderWidth: 1,
-    borderRadius: 20,
-    paddingHorizontal: 15,
+    borderRadius: 25,  // larger radius for rounder corners
+    paddingHorizontal: 20,
     marginRight: 10,
-    backgroundColor: '#fff',
+    backgroundColor: '#f2f2f7',  // a light gray background
   },
   sendButton: {
     backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 20,  // more horizontal padding for better appearance
+    borderRadius: 25,
+    elevation: 2,  // for Android
+    shadowOpacity: 0.2, // for iOS
+    shadowRadius: 1.5,  // for iOS
+    shadowOffset: { height: 1, width: 0 },  // for iOS
   },
   sendButtonText: {
     color: '#fff',
+    fontSize: 18,  // made slightly larger
+    fontWeight: '500',  // made slightly less bold for a more modern look
   },
 });
+
 
 export default ChatScreen;
